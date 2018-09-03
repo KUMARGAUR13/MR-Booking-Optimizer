d7b070be71fbd4fb3d68c36b4e0d44d74174d4d6
I335295 <gaurav.kumar13@sap.com>
2018-09-03 05:17:36 +0000

    test

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("MeetingRoomOptimizer.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("MeetingRoomOptimizer.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
